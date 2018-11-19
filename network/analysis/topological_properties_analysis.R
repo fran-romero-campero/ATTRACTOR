addc1e93c133e37d856670ed7700307c88150df5
Fran Romero-Campero <fran@us.es>
2018-11-19 09:00:44 +0100

    Update network representation file to include regulators. Add visualization of genes with multiple regulators

--- network/analysis/topological_properties_analysis.R
+++ network/analysis/topological_properties_analysis.R
@@ -8,8 +8,8 @@ atha.graph <- read.graph(file="../attractor.graphml", format = "graphml")
 vertex.names <- V(atha.graph)$name
 length(vertex.names)
 
+
 ## Initialise vectors to store topological parameters
-atha.neighbors <- vector(mode="character",length=length(vertex.names))
 atha.indegree <- vector(mode="numeric",length=length(vertex.names))
 atha.outdegree <- vector(mode="numeric",length=length(vertex.names))
 atha.trans <- vector(mode="numeric",length=length(vertex.names))
@@ -17,19 +17,29 @@ atha.close <- vector(mode="numeric",length=length(vertex.names))
 atha.between <- vector(mode="numeric",length=length(vertex.names))
 atha.eccent <- vector(mode="numeric",length=length(vertex.names))
 
+## Initialize matrix to represent the regulators for each gene
+atha.neighbors <- vector(mode="character",length=length(vertex.names))
+
+tfs <- c("CCA1","LHY", "TOC1", "PRR5", "PRR7", "PRR9", "PHYA","PHYB",
+         "CRY2","FHY1","LUX","PIF3","PIF4","PIF5","ELF4","ELF3")
+
+tfs.agi <- c("AT2G46830", "AT1G01060", "AT5G61380", "AT5G24470", "AT5G02810", "AT2G46790",
+             "AT1G09570", "AT2G18790", "AT1G04400", "AT2G37678", "AT3G46640", "AT1G09530",
+             "AT2G43010", "AT3G59060", "AT2G40080", "AT2G25930")
+
+names(tfs) <- tfs.agi
+
+regulators <- matrix(0,ncol=length(tfs),nrow=length(vertex.names))
+rownames(regulators) <- vertex.names
+colnames(regulators) <- tfs
+
+
 ## Loop to compute the different topological parameters and the names of the nodes
 ## connect to curren node (regulators)
 for (i in 1:vcount(atha.graph))
 {
-  print(i)
-  if (length(neighbors(graph = atha.graph, v=vertex.names[i], mode="in")$name) == 0) 
-  {
-    atha.neighbors[i] <- NA
-  } else
-  {
-    atha.neighbors[i] <- capture.output(cat(neighbors(graph = atha.graph, v=vertex.names[i], mode="in")$name, sep = ","))
-  }
-  
+  regulators[i,tfs[neighbors(graph = atha.graph, v=vertex.names[i], mode="in")$name]] <- 1
+
   atha.indegree[i] <- degree(graph = atha.graph, v=vertex.names[i],mode = "in")
   atha.outdegree[i] <- degree(graph = atha.graph, v=vertex.names[i],mode = "out")
   atha.trans[i] <- transitivity(graph = atha.graph, type = "local", vids=vertex.names[i])
@@ -39,10 +49,10 @@ for (i in 1:vcount(atha.graph))
 }
 
 ## Generate data frame with the topological parameters
-node.topological.parameters <- data.frame(vertex.names, atha.neighbors, atha.indegree, 
+node.topological.parameters <- data.frame(vertex.names, atha.indegree, 
                                           atha.outdegree, atha.trans, 
                                           atha.close, atha.between, atha.eccent)
-colnames(node.topological.parameters) <- c("names", "regulators", "indegree", "outdegree",
+colnames(node.topological.parameters) <- c("names", "indegree", "outdegree",
                                            "transitivity", "closeness", "betweeness", "eccentricity")
 
 rownames(node.topological.parameters) <- vertex.names
@@ -60,6 +70,7 @@ nrow(attractor.network)
 head(node.topological.parameters[attractor.network$names,])
 
 attractor.network.topological.parameters <- cbind(attractor.network,
+                                                  regulators[as.vector(attractor.network$names),],
                                                   node.topological.parameters[as.vector(attractor.network$names),2:ncol(node.topological.parameters)])
 
 head(attractor.network.topological.parameters)
--- web_apps/attractor/app.R
+++ web_apps/attractor/app.R
@@ -1,10 +1,11 @@
 ## Load libraries
 library(shiny)
 library(ggplot2)
+library(org.At.tair.db)
+
 
 ## Load network
 network.data <- read.table(file="data/attractor_network_topological_parameters.tsv",header = TRUE,as.is=TRUE)
-head(network.data)
 
 ## Tranforming coordinates for a better visualization
 x.coord <- as.numeric(network.data$y.pos)
@@ -31,6 +32,16 @@ names(node.colors) <- NULL
 ## Extract gene ids
 genes <- sort(network.data$name)
 
+## Load all and circadian genes
+my.key <- keys(org.At.tair.db, keytype="ENTREZID")
+my.col <- c("SYMBOL", "TAIR")
+columns(org.At.tair.db)
+alias2symbol.table <- select(org.At.tair.db, keys=my.key, columns=my.col, keytype="ENTREZID")
+alias2symbol.table <- subset(alias2symbol.table, genes %in% TAIR)
+alias <- alias2symbol.table$SYMBOL
+names(alias) <- alias2symbol.table$TAIR
+alias[is.na(alias)] <- "" 
+genes.selectize <- paste(names(alias), alias, sep=" - ")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -46,7 +57,7 @@ ui <- fluidPage(
         ## Select a few genes
         selectizeInput(inputId = "selected.genes",
                        label = "Gene ID",
-                       choices = genes,
+                       choices = genes.selectize,
                        selected = "AT1G22770",
                        multiple = TRUE),
         ## Button to trigger selections based on gene ID
@@ -70,7 +81,13 @@ AT4G16780"),
         sliderInput(inputId = "degree_range", label = h3("Degree Range"), min = 0, 
                     max = 11, value = c(2, 4)),
         actionButton(inputId = "button_degree",label = "Select Genes"),
-
+        
+        checkboxGroupInput(inputId = "selected.tfs",
+                           label = "Select Transcription Factors:",
+                           choices = list("CCA1","LHY", "TOC1", "PRR5", "PRR7", "PRR9", "PHYA","PHYB",
+                                          "CRY2","FHY1","LUX","PIF3","PIF4","PIF5","ELF3","ELF4"),
+                           inline = TRUE,width = "100%"),
+        actionButton(inputId = "button_tfs",label = "Select Genes"),
         width = 3 
       ),
       
@@ -96,11 +113,12 @@ server <- function(input, output) {
             axis.text = element_blank(),
             axis.ticks.y = element_blank()) + 
       geom_point(color=node.colors,size=1)
-  },height = 600)
+  },height = 700)
   
   selected_gene_id <- eventReactive(input$button_gene_id, {
     print("aquí llego 0")
-    selected.genes.df <- subset(network.data, names %in% input$selected.genes)
+    selected.genes.agi <- as.vector(unlist(as.data.frame(strsplit(input$selected.genes," - "))[1,]))
+    selected.genes.df <- subset(network.data, names %in% selected.genes.agi)
     print(selected.genes.df)
 #    return(selected.genes.df)
   })
@@ -109,8 +127,8 @@ server <- function(input, output) {
   ## Visualization of selected genes by ID
   observeEvent(input$button_gene_id, {
     print("aquí llego 1")
-    
-    selected.genes.df <- subset(network.data, names %in% input$selected.genes)
+    selected.genes.agi <- as.vector(unlist(as.data.frame(strsplit(input$selected.genes," - "))[1,]))
+    selected.genes.df <- subset(network.data, names %in% selected.genes.agi)
     selected.nodes.colors <- selected.colors[selected.genes.df$cluster.classification]
     
     print(selected.genes.df)
@@ -126,7 +144,7 @@ server <- function(input, output) {
         axis.ticks.y = element_blank()) + 
         geom_point(color=node.colors,size=1) +
         geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=3, fill=selected.nodes.colors,colour="black",pch=21)
-      },height = 600)
+      },height = 700)
   })
 
 
@@ -153,7 +171,7 @@ server <- function(input, output) {
               axis.ticks.y = element_blank()) + 
         geom_point(color=node.colors,size=1) +
         geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
-    },height = 600)
+    },height = 700)
   })
   
   ## Visualization of selected genes according to their degree
@@ -178,9 +196,43 @@ server <- function(input, output) {
               axis.ticks.y = element_blank()) + 
         geom_point(color=node.colors,size=1) +
         geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
-    },height = 600)
+    },height = 700)
   })
 
+  ## Visualization of selected genes according to their degree
+  observeEvent(input$button_tfs, {
+    print("aquí llego 4")
+
+    if(length(input$selected.tfs) == 1)
+    {
+      gene.selection <- network.data[,input$selected.tfs] == 1
+      sum(gene.selection)
+    } else if(length(input$selected.tfs) > 1)
+    {
+      gene.selection <- rowSums(network.data[,input$selected.tfs]) == length(input$selected.tfs)
+      
+    }
+    selected.genes.df <- network.data[gene.selection,]
+    selected.nodes.colors <- selected.colors[selected.genes.df$cluster.classification]
+    
+    print(selected.genes.df)
+    print(selected.nodes.colors)
+    
+    output$networkPlot <- renderPlot({
+      ggplot(network.data, aes(x.pos,y.pos)) + 
+        theme(panel.background = element_blank(), 
+              panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(),
+              axis.title = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks.y = element_blank()) + 
+        geom_point(color=node.colors,size=1) +
+        geom_point(data = selected.genes.df,aes(x.pos,y.pos), size=4, fill=selected.nodes.colors,colour="black",pch=21)
+    },height = 700)
+  })
+  
+  
+  
 }
 
 # Run the application 
