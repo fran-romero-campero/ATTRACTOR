b8023ecfb6fcfa8389b18ff725bad29a3d9f13d9
Francisco J. Romero-Campero <fran@us.es>
2019-02-07 21:32:55 +0100

    Progress in creating adjacency matrix with ZT for each TF

--- web_apps/peak_visualizer/data/targets_in_network/targets_to_intersect/adjacency_matrix_regulators_with_zt.R
+++ web_apps/peak_visualizer/data/targets_in_network/targets_to_intersect/adjacency_matrix_regulators_with_zt.R
@@ -55,33 +55,65 @@ lhy.zt02 <- read.table(file="LHY_targets_in_network.txt", as.is=T, header=F)[[1]
 adjacency.matrix[lhy.zt02,"LHY_ZT02"] <- 1
 
 ## TOC1_ZT15
+toc1.zt15 <- read.table(file="TOC1_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[toc1.zt15,"TOC1_ZT15"] <- 1
 
 ## PRR5_ZT10
+prr5.zt10 <- read.table(file="PRR5_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr5.zt10,"PRR5_ZT10"] <- 1
 
-##PRR7_ZT12
+## PRR7_ZT12
+prr7.zt12 <- read.table(file="PRR7_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr7.zt12,"PRR7_ZT12"] <- 1
 
-##PRR9_ZT??
+## PRR9_ZT??
+prr9.zt <- read.table(file="PRR9_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr9.zt,"PRR9_ZT??"] <- 1
 
 ## PHYA_ZT00
+phya.zt00 <- read.table(file="PHYA_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[phya.zt00,"PHYA_ZT00"] <- 1
 
 ## PHYB_ZT00
+phyb.zt00 <- read.table(file="PHYB_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[phyb.zt00,"PHYB_ZT00"] <- 1
 
-##CRY2_ZT08
+## CRY2_ZT08
+cry2.zt08 <- read.table(file="CRY2_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[cry2.zt08,"CRY2_ZT08"] <- 1
 
-##FHY1_ZT04
+## FHY1_ZT04
+fhy1.zt04 <- read.table(file="FHY1_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[fhy1.zt04,"FHY1_ZT04"] <- 1
 
-##LUX_ZT10
+## LUX_ZT10
+lux.zt10 <- read.table(file="LUX_ZT10_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[lux.zt10,"LUX_ZT10"] <- 1
 
 ## LUX_ZT12
+lux.zt12 <- read.table(file="LUX_ZT12_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[lux.zt12,"LUX_ZT12"] <- 1
 
 ## PIF3_ZT08
+pif3.zt08 <- read.table(file="PIF3_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif3.zt08,"PIF3_ZT08"] <- 1
 
 ## PIF4_ZT04
+pif4.zt04 <- read.table(file="PIF4_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif4.zt04,"PIF4_ZT04"] <- 1
 
 ## PIF5_ZT04
+pif5.zt04 <- read.table(file="PIF5_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif5.zt04,"PIF5_ZT04"] <- 1
 
 ## ELF4_ZT10
+elf4.zt10 <- read.table(file="ELF4_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf4.zt10,"ELF4_ZT10"] <- 1
 
 ## ELF3_ZT00
+elf3.zt00 <- read.table(file="ELF3_ZT00_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf3.zt00,"ELF3_ZT00"] <- 1
 
 ## ELF3_ZT04
+elf3.zt04 <- read.table(file="ELF3_ZT04_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf3.zt04,"ELF3_ZT04"] <- 1
