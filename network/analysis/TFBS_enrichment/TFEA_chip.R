db33bf7f057d402e90d2fa49857898c01f02fa94
pedrodelosreyes <pedro.reyes@ibvf.csic.es>
2019-02-27 13:06:52 +0100

    Advances in TFBS enrichment: create list of converted bed to granges

--- network/analysis/TFBS_enrichment/TFEA_chip.R
+++ network/analysis/TFBS_enrichment/TFEA_chip.R
@@ -8,8 +8,15 @@ library(TFEA.ChIP)
 # “Cell Type”, “Treatment”, “Antibody”, and “TF”.
 myMetadata <- read.table(file = "metadata.txt", header = T)
 head(myMetadata)
-# bed <- read.table(file = "bed_files/PIF3_peaks.narrowPeak")
-# head(bed)
+bed <- read.table(file = "bed_files/CCA1_peaks.narrowPeak")
+head(bed)
+
+#Convert to narrowpeak format (ten columns)
+# extra.columns <- matrix(nrow = nrow(bed), ncol = 7)
+# new.bed <- cbind(bed, extra.columns)
+# write.table(new.bed, file="bed_files/CCA1_new_peaks.narrowPeak", sep = "\t", 
+#             col.names = FALSE, row.names = FALSE)
+
 
 
 txt2GR(fileTable = bed, format = "narrowpeak", fileMetaData = myMetadata)
@@ -20,6 +27,7 @@ format<-"narrowpeak"
 
 gr.list <- lapply(
   seq_along(File.list),
+  
   function( File.list, myMetaData, format ){
     
     tmp<-read.table( file = paste0(folder,File.list[i]), stringsAsFactors = FALSE )
@@ -31,6 +39,24 @@ gr.list <- lapply(
     return(ChIP.dataset.gr)
   },
   File.list = File.list,
-  myMetadata = myMetadata,
+  myMetaData = myMetadata,
   format = format
 )
+
+txt2GR.fun <- function( File.list, myMetaData, format ){
+  
+  tmp<-read.table( file = paste0(folder,File.list[i]), stringsAsFactors = FALSE )
+  
+  file.metadata <- myMetadata[ myMetadata$Name == File.list[i], ]
+  
+  ChIP.dataset.gr<-txt2GR(tmp, format, file.metadata)
+  
+  return(ChIP.dataset.gr)
+}
+
+for (i in 1:length(File.list))
+{
+  gr.list[i] <- txt2GR.fun(File.list = File.list, myMetaData = myMetadata, format="narrowpeak")
+
+}
+
