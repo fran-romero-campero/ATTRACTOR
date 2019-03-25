e398ea692f44f1e72b14e6f6de21c3d147c6c5a7
Fran Romero-Campero <fran@us.es>
2019-03-25 15:54:20 +0100

    Homer like TFBS start

---
+++ network/analysis/TFBS_enrichment/homer_like/generate_background.R
@@ -0,0 +1,74 @@
+library(seqinr)
+library(TxDb.Athaliana.BioMart.plantsmart28)
+promoter.length <- 1000
+downstream.promoter <- 500
+
+txdb <- TxDb.Athaliana.BioMart.plantsmart28
+
+network.data <- read.table(file="../../../../web_apps/attractor_dev/data/attractor_network_representation.tsv",header = TRUE,as.is=TRUE,sep="\t",quote = "")
+head(network.data)
+network.genes <- network.data$names
+
+#genes.data <- subset(genes(txdb,columns=c("tx_id", "tx_name","gene_id")), seqnames %in% 1:5)
+genes.data <- subset(genes(txdb,columns=c("tx_id", "tx_name","gene_id")), seqnames %in% 1:5)
+genes.tss <- resize(genes.data, width=1, fix='start')
+genes.tss <- as.data.frame(genes.tss)
+genes.tss  <- genes.tss[network.genes,]
+head(genes.tss)
+
+genes.promoters.coordinates <- genes.tss[,c(1:3,5)]
+head(genes.promoters.coordinates)
+
+for(i in 1:nrow(genes.promoters.coordinates))
+{
+  print(i)
+  current.strand <- as.character(genes.promoters.coordinates$strand[i])
+  
+  if(current.strand == "+")
+  {
+    genes.promoters.coordinates$start[i] <- genes.promoters.coordinates$start[i] - 1000
+    genes.promoters.coordinates$end[i] <- genes.promoters.coordinates$end[i] + 500
+  } else if(current.strand == "-")
+  {
+    genes.promoters.coordinates$start[i] <- genes.promoters.coordinates$start[i] - 500
+    genes.promoters.coordinates$end[i] <- genes.promoters.coordinates$end[i] + 1000
+  } else
+  {
+    print("no strand!!!!")
+  }
+}
+
+chr1 <- getSequence(read.fasta(file = "../../../../web_apps/peak_visualizer/data/athaliana_genome/chr1.fa",seqtype = "AA"))[[1]]
+chr2 <- getSequence(read.fasta(file = "../../../../web_apps/peak_visualizer/data/athaliana_genome/chr2.fa",seqtype = "AA"))[[1]]
+chr3 <- getSequence(read.fasta(file = "../../../../web_apps/peak_visualizer/data/athaliana_genome/chr3.fa",seqtype = "AA"))[[1]]
+chr4 <- getSequence(read.fasta(file = "../../../../web_apps/peak_visualizer/data/athaliana_genome/chr4.fa",seqtype = "AA"))[[1]]
+chr5 <- getSequence(read.fasta(file = "../../../../web_apps/peak_visualizer/data/athaliana_genome/chr5.fa",seqtype = "AA"))[[1]]
+
+
+background.sequences <- vector(mode = "list",length = nrow(genes.promoters.coordinates))
+
+for(i in 1:nrow(genes.promoters.coordinates))
+{
+  print(i)
+  current.chr <- as.numeric(genes.promoters.coordinates$seqnames[i])
+  
+  if(current.chr == 1)
+  {
+    background.sequences[[i]] <- chr1[genes.promoters.coordinates$start[i]:genes.promoters.coordinates$end[i]]
+  } else if (current.chr == 2)
+  {
+    background.sequences[[i]] <- chr2[genes.promoters.coordinates$start[i]:genes.promoters.coordinates$end[i]]
+  } else if (current.chr == 3)
+  {
+    background.sequences[[i]] <- chr3[genes.promoters.coordinates$start[i]:genes.promoters.coordinates$end[i]]
+  } else if (current.chr == 4)
+  {
+    background.sequences[[i]] <- chr4[genes.promoters.coordinates$start[i]:genes.promoters.coordinates$end[i]]
+  } else if (current.chr == 5)
+  {
+    background.sequences[[i]] <- chr5[genes.promoters.coordinates$start[i]:genes.promoters.coordinates$end[i]]
+  }
+}
+
+write.fasta(sequences = background.sequences,names = rownames(genes.promoters.coordinates),
+            file.out = paste(paste(c("background",promoter.length,downstream.promoter,nrow(genes.promoters.coordinates)), collapse="_"),".fa",sep=""))      
