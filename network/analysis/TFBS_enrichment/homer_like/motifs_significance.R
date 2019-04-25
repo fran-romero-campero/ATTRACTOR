a052ad3e641bc712271d7dd39d267f3207ea1828
pedrodelosreyes <pedro.reyes@ibvf.csic.es>
2019-04-25 16:39:55 +0200

    Apply TFBS enrichment analysis (HOMER-like) over a set of genes. The groups of genes tested are the intersections between tge targets of two TFs and a cluster of genes. Generate tables with info

--- network/analysis/TFBS_enrichment/homer_like/motifs_significance.R
+++ network/analysis/TFBS_enrichment/homer_like/motifs_significance.R
@@ -11,7 +11,7 @@
 ## x number of genes WITH the motif in the gene selection
 
 ## Input parameters
-input <- list(promoter_length=500,downstream_length=0,score="100",motif_significance=0.05,enrichment_threshold=2)
+input <- list(promoter_length=1000,downstream_length=0,score="90",motif_significance=0.05,enrichment_threshold=2)
 
 file.precomputed <- paste0(c("precomputed_",input$promoter_length,"_",
                            input$downstream_length, "_",
@@ -25,6 +25,7 @@ n <- nrow(precomputed.result) - m
 ## Load file with selection, compute its size (k) and number of ocurrences (x).
 target.genes <- read.table(file = "peak_ZT0_trough_ZT12.txt",as.is = T)[[1]]
 target.genes <- intersect(rownames(precomputed.result),target.genes)
+
 
 k <- length(target.genes)
 x <- colSums(precomputed.result[target.genes,] > 0)
@@ -122,11 +123,6 @@ p.values["GBF3"]
 ## Compute significance based on randomisations
 
 
-
-
-
-
-
 target.motif.multiplicity <- colSums(background[target.genes,])
 
 number.randomisations <- 1000
@@ -156,7 +152,64 @@ for(i in 1:ncol(background))
 
 q.values <- p.adjust(p = p.values,method = "BH")
 
-
 colnames(background)[q.values < 0.001]
+
+
+
+######--Apply hypergeometric distribution to perform enrichment analysis-----######
+######--over several set of genes and store data in tables (Pedro) --####
+
+tfs.intersections.data <- read.table(file = "../../intersections/filtered_significant_results_1.tsv",as.is = T, header = TRUE)
+head(tfs.intersections.data)
+
+
+
+for (j in 1:nrow(tfs.intersections.data)) 
+{
+  print(j)
+  target.genes <- strsplit(x = tfs.intersections.data$genes[j], split = ",")[[1]]
+  k <- length(target.genes)
+  x <- colSums(precomputed.result[target.genes,] > 0)
+  ## Compute p-values for enrichment aocording to a hypergeometric distribution
+  p.values <- vector(mode="numeric", length=length(x))
+  names(p.values) <- colnames(precomputed.result)
+  
+  for(i in 1:length(x))
+  {
+    p.values[i] <- phyper(q = x[i] - 1, m = m[i], n = n[i], k = k, lower.tail = F)
+  }
+  
+  # which(p.values < input$motif_significance)
+  # p.values[which(p.values < input$motif_significance)]
+  
+  ## Adjust p-values using Benjamini Hochberg
+  q.values <- p.adjust(p = p.values,method = "BH")
+  names(q.values) <- names(p.values)
+  
+  which(q.values < input$motif_significance)
+  
+  ## Compute enrichments
+  enrichments <- (x / k) / (m / nrow(precomputed.result))
+  
+  
+  ## Final motifs
+  sig.enrich.motifs <- names(which(q.values < input$motif_significance & enrichments > input$enrichment_threshold))
+  
+  final.q.values <- q.values[which(q.values < input$motif_significance & enrichments > input$enrichment_threshold)]
+  final.p.values <- p.values[which(q.values < input$motif_significance & enrichments > input$enrichment_threshold)]
+  final.enrichments <- enrichments[which(q.values < input$motif_significance & enrichments > input$enrichment_threshold)]
+  
+  ##Store data
+  
+  store.data <- data.frame(sig.enrich.motifs, final.p.values, final.q.values, final.enrichments) 
+  write.table(store.data, sep = "\t", row.names = FALSE,
+              file = paste0("TFBS_enrichment_intersections/TFBS_enrichment_of_", tfs.intersections.data$tf1[j], "_",
+                            tfs.intersections.data$tf2[j], "_",
+                            tfs.intersections.data$peak.zt[j],"_", 
+                            tfs.intersections.data$trough.zt[j], "_", "intersection.txt")
+                           
+              )       
+  
+}
 
 
