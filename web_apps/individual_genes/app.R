569eb33870fac4200cbbd8944d2a5c226095b534
pedrodelosreyes <pedro.reyes@ibvf.csic.es>
2019-05-16 13:09:51 +0200

    Add ui section of peak visualizer in individual genes app

--- web_apps/individual_genes/app.R
+++ web_apps/individual_genes/app.R
@@ -1,4 +1,141 @@
 library(shiny)
+## Load libraries
+library(shiny)
+library(ChIPpeakAnno)
+library(rtracklayer)
+library(TxDb.Athaliana.BioMart.plantsmart28)
+library(Biostrings)
+library(seqinr)
+library(org.At.tair.db)
+
+## Load and extract Arabidopsis thaliana annotation regarding genes, exons and cds 
+txdb <- TxDb.Athaliana.BioMart.plantsmart28
+genes.data <- subset(genes(txdb), seqnames %in% c("1","2","3","4","5")) ## only nuclear genes are considered
+genes.data <- as.data.frame(genes.data)
+exons.data <- as.data.frame(exons(txdb))
+cds.data <- as.data.frame(cds(txdb))
+
+## Load all and circadian genes
+my.key <- keys(org.At.tair.db, keytype="ENTREZID")
+my.col <- c("SYMBOL", "TAIR")
+alias2symbol.table <- select(org.At.tair.db, keys=my.key, columns=my.col, keytype="ENTREZID")
+alias <- alias2symbol.table$SYMBOL
+names(alias) <- alias2symbol.table$TAIR
+alias[is.na(alias)] <- "" 
+genes <- paste(names(alias), alias, sep=" - ")
+
+circadian.genes <- read.table(file="data/genes_info/gene_info.txt",header=FALSE,col.names = c("gene.name","symbol"),fill = TRUE,as.is=TRUE)
+circadian.genes <- paste(circadian.genes$gene.name, circadian.genes$symbol, sep = " - ")
+
+## Color vectors
+line.colors <- c("blue","red", "darkgreen","black","#663300","#99003d","#b3b300","#4d0039","#4d2600","#006666","#000066","#003300","#333300","#660066")
+area.colors <- c("skyblue","salmon", "lightgreen","lightgrey","#ffcc99","#ff99c2","#ffffb3","#ffe6f9","#ffe6cc","#80ffff","#b3b3ff","#99ff99","#e6e600","#ffb3ff")
+
+## Load chromosome sequences
+chr1 <- getSequence(read.fasta(file = "data/athaliana_genome/chr1.fa",seqtype = "AA"))[[1]]
+chr2 <- getSequence(read.fasta(file = "data/athaliana_genome/chr2.fa",seqtype = "AA"))[[1]]
+chr3 <- getSequence(read.fasta(file = "data/athaliana_genome/chr3.fa",seqtype = "AA"))[[1]]
+chr4 <- getSequence(read.fasta(file = "data/athaliana_genome/chr4.fa",seqtype = "AA"))[[1]]
+chr5 <- getSequence(read.fasta(file = "data/athaliana_genome/chr5.fa",seqtype = "AA"))[[1]]
+
+## Function to compute the reverse complement
+reverse.complement <- function(dna.sequence)
+{
+  return(c2s(comp(rev(s2c(dna.sequence)),forceToLower = FALSE)))
+}
+
+## Load Position Weight Matrices
+## Open file connection
+con <- file("data/jaspar_motifs/pfm_plants_20180911.txt",open = "r")
+
+## Empty list for storing PWM
+motifs.pwm <- vector(mode="list",length = 453)
+motif.ids <- vector(mode="character",length=453)
+motif.names <- vector(mode="character",length=453)
+
+## Load 64 PWM
+for(j in 1:453)
+{
+  ## First line contains motif id and name
+  first.line <- readLines(con,1)
+  
+  motif.ids[j] <- strsplit(first.line,split=" ")[[1]][1]
+  motif.names[j] <- strsplit(first.line,split=" ")[[1]][2]
+  
+  ## Next four line contians probabilites for each nucleotide
+  a.row <- as.numeric(strsplit(readLines(con,1),split="( )+")[[1]])
+  c.row <- as.numeric(strsplit(readLines(con,1),split="( )+")[[1]])
+  g.row <- as.numeric(strsplit(readLines(con,1),split="( )+")[[1]])
+  t.row <- as.numeric(strsplit(readLines(con,1),split="( )+")[[1]])
+  
+  ## Construct PWM
+  motif.pwm <- matrix(nrow = 4,ncol=length(a.row))
+  
+  motif.pwm[1,] <- a.row
+  motif.pwm[2,] <- c.row 
+  motif.pwm[3,] <- g.row
+  motif.pwm[4,] <- t.row
+  
+  rownames(motif.pwm) <- c("A","C","G","T")
+  
+  motifs.pwm[[j]] <- prop.table(motif.pwm,2)
+}
+
+## Close file connection
+close(con)
+
+## Naming list with PWM
+names(motifs.pwm) <- motif.names
+names(motif.ids) <- motif.names
+
+bigwig.files <- c("data/bw_files/PHYA.bw",
+                  "data/bw_files/PHYB_FLAG_27_1.bw",
+                  "data/bw_files/PRR5_1.bw",
+                  "data/bw_files/TOC1.bw",
+                  "data/bw_files/CCA1_ZT02.bw",
+                  "data/bw_files/CCA1_ZT14_1.bw",
+                  "data/bw_files/LHY_1.bw",
+                  "data/bw_files/CRY2.bw",
+                  "data/bw_files/FHY1.bw",
+                  "data/bw_files/LUX_ZT10.bw",
+                  "data/bw_files/LUX_ZT12.bw",
+                  "data/bw_files/PIF3.bw",
+                  "data/bw_files/PIF4.bw",
+                  "data/bw_files/PIF5.bw",
+                  "data/bw_files/PRR7.bw",
+                  "data/bw_files/PRR9_1.bw",
+                  "data/bw_files/ELF3_ZT0.bw",
+                  "data/bw_files/ELF3_ZT4.bw",
+                  "data/bw_files/ELF4.bw")
+
+names(bigwig.files) <- c("PHYA ZT00", "PHYB ZT00" ,"PRR5 ZT10", "TOC1 ZT15","CCA1 ZT02","CCA1 ZT14","LHY ZT02","CRY2 ZT08","FHY1 ZT04","LUX ZT10", "LUX ZT12","PIF3 ZT08","PIF4 ZT04","PIF5 ZT04","PRR7 ZT12","PRR9 ZT04","ELF3 ZT00", "ELF3 ZT04", "ELF4 ZT10")
+
+bed.files <- c("data/bed_files/PHYA_peaks.narrowPeak",
+               "data/bed_files/PHYB_peaks.narrowPeak",
+               "data/bed_files/PRR5_1_peaks.narrowPeak",
+               "data/bed_files/TOC1_1_peaks.narrowPeak",
+               "data/bed_files/CCA1_ZT02_peaks.narrowPeak",
+               "data/bed_files/CCA1_ZT14_peaks.narrowPeak",
+               "data/bed_files/LHY_1_peaks.narrowPeak",
+               "data/bed_files/CRY2_peaks.narrowPeak",
+               "data/bed_files/FHY1_peaks.narrowPeak",
+               "data/bed_files/LUX_ZT10_1_peaks.narrowPeak",
+               "data/bed_files/LUX_ZT12_1_peaks.narrowPeak",
+               "data/bed_files/PIF3_peaks.narrowPeak",
+               "data/bed_files/PIF4_peaks.narrowPeak",
+               "data/bed_files/PIF5_peaks.narrowPeak",
+               "data/bed_files/PRR7_peaks.narrowPeak",
+               "data/bed_files/PRR9_1_peaks.narrowPeak",
+               "data/bed_files/ELF3_ZT0_1_peaks.narrowPeak",
+               "data/bed_files/ELF3_ZT4_1_peaks.narrowPeak",
+               "data/bed_files/ELF4_1_peaks.narrowPeak")
+
+names(bed.files) <- c("PHYA ZT00", "PHYB ZT00" ,"PRR5 ZT10", "TOC1 ZT15","CCA1 ZT02","CCA1 ZT14","LHY ZT02","CRY2 ZT08","FHY1 ZT04","LUX ZT10", "LUX ZT12", "PIF3 ZT08","PIF4 ZT04","PIF5 ZT04","PRR7 ZT12","PRR9 ZT04","ELF3 ZT00", "ELF3 ZT04", "ELF4 ZT10")
+
+## TF binding sites colors and symbol shapes
+symbol.shapes <- c(17, 18, 19, 15)
+symbol.color <- c("blue", "red", "darkgreen", "magenta")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -14,11 +151,100 @@ ui <- fluidPage(
                    img(src='dribbbble.jpg', align = "center", width=200),
                    selectInput(inputId = "selected_gene",
                                                       label = "Select a gene", 
-                                                      choices = c("gen1","gen2","gen3"))),
+                                                      choices = c("gen1","gen2","gen3")),
+                   radioButtons(inputId = "selected_analysis", label = "Choose your analysis",
+                                choices = c("Peak Visualizer" = "peakvis", "Network Visualizer" = "networkvis"))),
             column(width = 8, 
-                   tabsetPanel(type = "tabs",
-                               tabPanel(title = "Network Visualizer", plotOutput(outputId = "plot")),
-                               tabPanel(title = "Peak Visualizer", plotOutput(outputId = "plot3")))
+                   # Only show this panel if the peak visualizer is selected
+                   conditionalPanel(condition = "input$selected_analysis = 'peakvis'",
+                                    tags$head(
+                                      tags$style(HTML("
+                                                      .shiny-output-error-validation {
+                                                      color: red;
+                                                      font-size: 20px;
+                                                      }
+                                                      "))
+                                      ),
+                                    
+                                    #shinythemes::themeSelector(),
+                                    # Application title
+                                    titlePanel("Peak Visualizer"),
+                                    
+                                    # Sidebar with a slider input for number of bins 
+                                    sidebarLayout(
+                                      fluidRow(
+                                        
+                                        column(wellPanel(
+                                          ## Selectize to choose target gene to represent
+                                          selectizeInput(inputId = "target.gene",
+                                                         label = "Gene",
+                                                         choices = genes,
+                                                         multiple = FALSE),
+                                          
+                                          ## Numeric input for promoter length
+                                          numericInput(inputId = "promoter.length",
+                                                       label = "Promoter Length",
+                                                       value = 2000,
+                                                       min = 500,
+                                                       max = 2000,
+                                                       step = 100),
+                                          
+                                          ## Numeric input for 5' length
+                                          numericInput(inputId = "fiveprime.length",
+                                                       label = "5' Length",
+                                                       value = 500,
+                                                       min = 100,
+                                                       max = 500,
+                                                       step = 100)),width=4),
+                                        
+                                        column(wellPanel(
+                                          ## Check box for the TF peaks to represent
+                                          checkboxGroupInput(inputId = "names.tfs",
+                                                             label = "Select Transcription Factors:",
+                                                             choices = list("PHYA ZT00", "PHYB ZT00", "ELF3 ZT00", "CCA1 ZT02", "LHY ZT02", "ELF3 ZT04","FHY1 ZT04", "PIF4 ZT04", "PIF5 ZT04", "PRR9 ZT04", "CRY2 ZT08", "PIF3 ZT08", "ELF4 ZT10", "PRR5 ZT10", "LUX ZT10", "PRR7 ZT12", "LUX ZT12","CCA1 ZT14", "TOC1 ZT15"),
+                                                             
+                                                             
+                                                             #list("CCA1 ZT02", "CCA1 ZT14","LHY", "TOC1", "PRR5", "PRR7", "PRR9", "PHYA","PHYB",
+                                                             #               "CRY2","FHY1","LUX","PIF3","PIF4","PIF5","ELF3 ZT00","ELF3 ZT04","ELF4"),
+                                                             inline = TRUE,width = "100%")),width=4),
+                                        
+                                        column(wellPanel(
+                                          ## Selectize to choose target gene to represent
+                                          selectizeInput(inputId = "selected.motifs",
+                                                         label = "Select Motifs",
+                                                         choices = motif.names,
+                                                         multiple = TRUE),
+                                          
+                                          ## Checkbox to select all available motifs
+                                          checkboxInput(inputId = "all.motifs",
+                                                        label = "Select All Motifs:",
+                                                        value = FALSE),
+                                          
+                                          ## Numeric input for PWM score
+                                          numericInput(inputId = "min.score.pwm", 
+                                                       label = "Minimum Score for Motif Identification:",
+                                                       value = 100, 
+                                                       min = 80,
+                                                       max = 100,
+                                                       step = 5)),width=4),
+                                        actionButton(inputId = "go",label = "GO")
+                                      ),
+                                      
+                                      # fluidRow(
+                                      #   column(
+                                      #     ## Action button to trigger plot drawing
+                                      #     actionButton(inputId = "go",label = "GO"),width=12)),
+                                      
+                                      # Show a plot of the generated distribution
+                                      fluidRow(
+                                        column(
+                                          plotOutput(outputId = "plot.to.chulo.to.wapo"),
+                                          width=12)
+                                      )
+                                    )
+                                      )
+                                    
+
                    ),
             column(width = 2, plotOutput(outputId = "plot2"))
         ),
---
+++ web_apps/individual_genes/data/targets_in_network/targets_to_intersect/adjacency_matrix_regulators_with_zt.R
@@ -0,0 +1,129 @@
+# R script for pre-processing 
+# Copyright (C) 2018  Francisco J. Romero-Campero, Pedro de los Reyes Rodríguez
+# Ana Belén Romero Losada
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors: Pedro de los Reyes Rodríguez
+#          Ana Belén Romero-Losada
+#          Francisco J. Romero-Campero
+# 
+# Contact: Francisco J. Romero-Campero - fran@us.es 
+# Date: February 2019
+
+## Check that the set of the targets is equal to the number of genes in 
+files <- list.files(path = ".")
+files <- files[-1]
+
+targets <- c()
+for(i in 1:length(files))
+{
+  targets <- c(targets, read.table(file=files[i],header=F, as.is=T)[[1]])
+}
+
+length(unique(targets))
+
+## Load network data
+network.data <- read.table(file="../../../../attractor_dev/data/attractor_network_representation.tsv",sep="\t",header=T,quote = "",as.is=T)
+head(network.data)
+nrow(network.data)
+
+adjacency.matrix <- matrix(0,ncol=19,nrow=nrow(network.data))
+rownames(adjacency.matrix) <- network.data$names
+colnames(adjacency.matrix) <- c("CCA1_ZT02", "CCA1_ZT14", "LHY_ZT02","TOC1_ZT15","PRR5_ZT10", "PRR7_ZT12", "PRR9_ZT??",
+                                "PHYA_ZT00", "PHYB_ZT00", "CRY2_ZT08", "FHY1_ZT04", "LUX_ZT10", "LUX_ZT12", 
+                                "PIF3_ZT08", "PIF4_ZT04", "PIF5_ZT04", "ELF4_ZT10", "ELF3_ZT00", "ELF3_ZT04")
+
+## CCA1 ZT02
+cca1.zt02 <- read.table(file="CCA1_ZT02_targets_in_network.txt",as.is=T,header=F)[[1]]
+adjacency.matrix[cca1.zt02,"CCA1_ZT02"] <- 1
+
+## CCA1 ZT14
+cca1.zt14 <- read.table(file="CCA1_ZT14_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[cca1.zt14,"CCA1_ZT14"] <- 1
+
+## LHY ZT02
+lhy.zt02 <- read.table(file="LHY_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[lhy.zt02,"LHY_ZT02"] <- 1
+
+## TOC1_ZT15
+toc1.zt15 <- read.table(file="TOC1_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[toc1.zt15,"TOC1_ZT15"] <- 1
+
+## PRR5_ZT10
+prr5.zt10 <- read.table(file="PRR5_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr5.zt10,"PRR5_ZT10"] <- 1
+
+## PRR7_ZT12
+prr7.zt12 <- read.table(file="PRR7_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr7.zt12,"PRR7_ZT12"] <- 1
+
+## PRR9_ZT??
+prr9.zt <- read.table(file="PRR9_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[prr9.zt,"PRR9_ZT??"] <- 1
+
+## PHYA_ZT00
+phya.zt00 <- read.table(file="PHYA_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[phya.zt00,"PHYA_ZT00"] <- 1
+
+## PHYB_ZT00
+phyb.zt00 <- read.table(file="PHYB_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[phyb.zt00,"PHYB_ZT00"] <- 1
+
+## CRY2_ZT08
+cry2.zt08 <- read.table(file="CRY2_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[cry2.zt08,"CRY2_ZT08"] <- 1
+
+## FHY1_ZT04
+fhy1.zt04 <- read.table(file="FHY1_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[fhy1.zt04,"FHY1_ZT04"] <- 1
+
+## LUX_ZT10
+lux.zt10 <- read.table(file="LUX_ZT10_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[lux.zt10,"LUX_ZT10"] <- 1
+
+## LUX_ZT12
+lux.zt12 <- read.table(file="LUX_ZT12_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[lux.zt12,"LUX_ZT12"] <- 1
+
+## PIF3_ZT08
+pif3.zt08 <- read.table(file="PIF3_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif3.zt08,"PIF3_ZT08"] <- 1
+
+## PIF4_ZT04
+pif4.zt04 <- read.table(file="PIF4_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif4.zt04,"PIF4_ZT04"] <- 1
+
+## PIF5_ZT04
+pif5.zt04 <- read.table(file="PIF5_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[pif5.zt04,"PIF5_ZT04"] <- 1
+
+## ELF4_ZT10
+elf4.zt10 <- read.table(file="ELF4_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf4.zt10,"ELF4_ZT10"] <- 1
+
+## ELF3_ZT00
+elf3.zt00 <- read.table(file="ELF3_ZT00_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf3.zt00,"ELF3_ZT00"] <- 1
+
+## ELF3_ZT04
+elf3.zt04 <- read.table(file="ELF3_ZT04_targets_in_network.txt", as.is=T, header=F)[[1]]
+adjacency.matrix[elf3.zt04,"ELF3_ZT04"] <- 1
+
+## Add columns to network representation and write new version
+network.representation.with.zts <- cbind(network.data,adjacency.matrix)
+head(network.representation.with.zts)
+
+rownames(network.representation.with.zts) <- NULL
+head(network.representation.with.zts)
+
+write.table(network.representation.with.zts, file="attractor_network_representation.tsv", sep = "\t", quote = FALSE,
+            row.names = FALSE)
