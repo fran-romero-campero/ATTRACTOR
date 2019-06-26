51c513a28277f7b17c87fdc0b0493f614b8271a0
Fran Romero-Campero <fran@us.es>
2019-06-26 07:19:37 +0200

    Fix bug in edge color in clock representation when only a TF was selected

--- web_apps/individual_genes/app.R
+++ web_apps/individual_genes/app.R
@@ -723,26 +723,22 @@ server <- function(input, output) {
     tfs.network <- graph.adjacency(adjmatrix = new.matrix, mode = "directed",weighted = TRUE)
     edge.weights <- E(tfs.network)$weight
     
-    if (length(input$selected.tfs) > 1)
+    ## Edge colors
+    for(k in 1:length(edge.weights))
     {
-      ## Edge colors
-      for(k in 1:length(edge.weights))
+      if(edge.weights[k] == 1)
       {
-        if(edge.weights[k] == 1)
-        {
-          E(tfs.network)$color[k] <- activator.color #"darkgreen"
-        } else if(edge.weights[k] == -1)
-        {
-          E(tfs.network)$color[k] <- repressor.color  #"darkred"
-        } else if(edge.weights[k] == 2)
-        {
-          print(1)
-          E(tfs.network)$color[k] <- neutral.color
-        }
+        E(tfs.network)$color[k] <- activator.color #"darkgreen"
+      } else if(edge.weights[k] == -1)
+      {
+        E(tfs.network)$color[k] <- repressor.color  #"darkred"
+      } else if(edge.weights[k] == 2)
+      {
+        print(1)
+        E(tfs.network)$color[k] <- neutral.color
       }
-      
     }
-        
+
     ## Vertex colors
     node.colors <- vector(mode="character",length=nrow(new.matrix))
     for(k in 1:(length(node.colors)-1))
