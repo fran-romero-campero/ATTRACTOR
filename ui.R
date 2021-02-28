2e94f8c69e884a6c31964ddd92e8c37ea5ebb9bf
fran-romero-campero <fran@us.es>
2021-02-28 20:18:51 +0100

    Change map with visits

--- ui.R
+++ ui.R
@@ -161,7 +161,8 @@ ui <- fluidPage(
       tags$br(),
       tags$br(),
       tags$div(align = "center", width=60,
-               HTML("<script type=\"text/javascript\" id=\"clstr_globe\" src=\"//cdn.clustrmaps.com/globe.js?d=_vn8-gT1zeKYdG0FCV7CsZkw0zisYiGXkw3ZoGonziU\"></script>")
+               #HTML("<script type=\"text/javascript\" id=\"clstr_globe\" src=\"//cdn.clustrmaps.com/globe.js?d=_vn8-gT1zeKYdG0FCV7CsZkw0zisYiGXkw3ZoGonziU\"></script>")
+               HTML("<script type=\"text/javascript\" src=\"//rf.revolvermaps.com/0/0/1.js?i=5lpcyfvr1gd&amp;s=220&amp;m=0&amp;v=false&amp;r=false&amp;b=000000&amp;n=false&amp;c=ff0000\" async=\"async\"></script>")
       )
     )
   ),
